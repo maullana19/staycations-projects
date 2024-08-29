e92c02e23be766c1cd532dc6dd55bc4348885494
maullana19 <dedemaulana162@gmail.com>
2024-08-29 08:56:30 +0700

    FirstCommit

---
+++ src/elements/Button/index.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Button from "./index";
+import { Router } from "react-router-dom";
+
+test("Should not allowed click button if isDisabled is present", () => {
+  const { container } = render(<Button isDisabled>Hello World</Button>);
+
+  expect(container.querySelector("span.disabled")).toBeInTheDocument();
+});
+
+test("Should render Loading/spinner", () => {
+  const { container, getByText } = render(<Button isLoading></Button>);
+
+  expect(getByText(/loading/i)).toBeInTheDocument();
+  expect(container.querySelector("span")).toBeInTheDocument();
+});
+
+test("Should render <a> tag", () => {
+  const { container } = render(<Button type="link" isExternal></Button>);
+
+  expect(container.querySelector("a")).toBeInTheDocument();
+});
